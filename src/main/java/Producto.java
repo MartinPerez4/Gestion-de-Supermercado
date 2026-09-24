16642bac576333eb8bf9e003e199f4d016177240
Cristopasco <cristopher.tapascot@uqvirtual.edu.co>
2026-09-23 20:46:50 -0500

    Agregar a categoria al constructor

--- src/main/java/Producto.java
+++ src/main/java/Producto.java
@@ -4,11 +4,12 @@ public class Producto {
     private double precioUnitario;
     private int cantidadDisponible;
 
-    public Producto (String codigo, String nombre, double precioUnitario, int cantidadDisponible) {
+    public Producto (String codigo, String nombre, double precioUnitario, int cantidadDisponible, CategoriaProducto categoria) {
         this.codigo = codigo;
         this.nombre = nombre;
         this.precioUnitario = precioUnitario;
         this.cantidadDisponible = cantidadDisponible;
+        this.categoriaProducto = categoria;
     }
     public CategoriaProducto categoriaProducto;
 
