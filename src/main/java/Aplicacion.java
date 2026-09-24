6a76c0173e763ff7fa8b82c94bc6e9b8242a754b
Eduardozzzzz <eduardo.pradao@uqvirtual.edu.co>
2026-09-23 21:08:36 -0500

    Menu base (comentarios para empezar basarse y hacerlo con joptionpane) y switch case

--- src/main/java/Aplicacion.java
+++ src/main/java/Aplicacion.java
@@ -4,9 +4,34 @@ public class Aplicacion {
         int opcion;
         do{
             //Menu ou shit
+            //
+            //Menu administrador/supermercado
+            //1. Agregar producto
+            //2. Mostrar productos disponibles
+            //3. Actualizar productos disponibles
+            //4. Eliminar productos
+            //
+            //5. Agregar clientes
+            //6. Mostrar clientes registrados
+            //7. Actualizar clientes registrados
+            //8. Eliminar clientes
+            //
+            //9. Mostrar todas las compras
+            //10. Eliminar compra
+            //
+            //Menu cliente
+            //11. Realizar compra
+            //12. Mostrar historial de compras
+            //13. Modificar compra
+            //
+            //0. Salir del programa
             //Switch case
             switch(opcion){
+                case 0:
+                    //Cerrando programa
+                    break;
                 case 1:
+
                     break;
                 case 2:
                     break;
